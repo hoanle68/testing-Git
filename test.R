c5c5d1afbf96973e9339afb0d5ed9756f41ec5b8
H. Le <hoanngocle@gmail.com>
2023-10-23 11:00:42 -0400

    submit first change on script

---
+++ test.R
@@ -0,0 +1,15 @@
+# I want to iinstall a gapminder package and "turn it on"
+
+install.packages("gapminder")
+library(gapminder)
+
+install.packages("tidyverse")
+library(tidyverse)
+
+read_data <- gapminder
+
+
+#add som folder
+dir.create("images")
+dir.create("docs")
+dir.create("figures")
